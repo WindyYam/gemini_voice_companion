1c46264ff86696eb3ca65131e24b6d293dd11ccf
zhenya.yang <zhenya.y@lx-group.com.au>
2025-02-21 11:31:40 +1100

    small error

--- scripts/text_stream_to_stream.py
+++ scripts/text_stream_to_stream.py
@@ -279,7 +279,7 @@ class TextStreamToAudioStream(TextToAudioStream):
         self.player.buffer_manager.clear_buffer()
 
     def is_still_playing(self):
-        playing = not self.player.buffer_manager.audio_buffer.qsize() > 0
+        playing = not (self.player.buffer_manager.audio_buffer.qsize() == 0)
         # in rare case sentence_queue is not created yet
         try:
             playing = playing or self.sentence_queue.qsize() > 0
